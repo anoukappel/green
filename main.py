40ac3f11531b49deb6acd3067ec45985f19cc1c0
anoukappel <anouk-appel@hotmail.com>
2023-01-30 10:54:45 +0100

    comments aangepast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from code.classes import battery, district, house, model
-from code.algorithms import random
+from code.algorithms import random, hillclimber
 from code.solutions import save_solution
 from code.visualisatie import histogram, scatterplot
 
@@ -16,20 +16,20 @@ if __name__ == "__main__":
     # object district aanmaken bestaande uit batteries en huizen data
     district_test = district.District(file)
 
-    smallest_solution, list_cable_lengths = random.run(10, district_test)
-
-    # print(len(smallest_solution.cables))
-
-    # Plotting histogram greedy + random
-    histogram.plotting_histogram(list_cable_lengths)
-
-    # average = mean(list_cable_lengths)
-    # print(f"Average sum of cables using random + greedy algorithm is: {average}" )
-
-    # Showing plot of all batteries
-    scatterplot.show_scatterplot(smallest_solution, multiple_plots = False)
-    # Showing a plot of each battery
-    scatterplot.show_scatterplot(smallest_solution)
+    # smallest_solution, list_cable_lengths = random.run(10, district_test)
+    #
+    # # print(len(smallest_solution.cables))
+    #
+    # # Plotting histogram greedy + random
+    # histogram.plotting_histogram(list_cable_lengths)
+    #
+    # # average = mean(list_cable_lengths)
+    # # print(f"Average sum of cables using random + greedy algorithm is: {average}" )
+    #
+    # # Showing plot of all batteries
+    # scatterplot.show_scatterplot(smallest_solution, multiple_plots = False)
+    # # Showing a plot of each battery
+    # scatterplot.show_scatterplot(smallest_solution)
 
     """ Hillclimber algortihm """
     model_test = model.Model(district_test)
